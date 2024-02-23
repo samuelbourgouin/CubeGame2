5dcffa6def571fe4ba0fd8768d1b55e8e53ba65e
samuelbourgouin <samuelbourgouin@hotmail.com>
2024-02-22 20:32:37 -0500

    camera fixed

--- scenes/coin.gd
+++ scenes/coin.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
 @onready var game_manager = %gameManager
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 func _on_body_entered(body):
 	if (body.name == "CharacterBody2D"):
 		queue_free()
 		game_manager.add_coin()
+		animated_sprite_2d.animation = "effect"
