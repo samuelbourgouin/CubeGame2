824ffc9a3d510e991a8eaecb1266262e80534073
samuelbourgouin <samuelbourgouin@hotmail.com>
2024-02-24 22:55:03 -0500

    placeholder menu and level changer

--- scenes/character/main_character.gd
+++ scenes/character/main_character.gd
@@ -1,47 +1,46 @@
 extends CharacterBody2D
 
 
-const SPEED = 400.0
-const JUMP_VELOCITY = -900.0
+@export var SPEED = 400.0
+@export var JUMP_VELOCITY = -900.0
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var lastDirection = Vector2.ZERO
-
+var was_in_air = false
 func _physics_process(delta):
-	# Animation
-	if abs(velocity.x) > 1:
-		animated_sprite_2d.animation = "running"
-	else:
-		animated_sprite_2d.animation = "default"
 	
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		animated_sprite_2d.animation = "jumping"
-		if velocity.y > 0:
-			animated_sprite_2d.animation = "fall"
-	# Handle jump.
+	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
-	# handle droping through one way collision
 	if(Input.is_action_just_pressed("down") && is_on_floor()):
 		position.y += 1
 	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 		lastDirection = Vector2(direction, 0)
 	else:
 		velocity.x = move_toward(velocity.x, 0, 150)
-
+	
+	update_animation()
 	move_and_slide()
 	
 	var isLeft = lastDirection.x < 0
 	animated_sprite_2d.flip_h = isLeft
-	
 
+func update_animation():
+	if abs(velocity.x) > 1:
+		animated_sprite_2d.play("running")
+	else:
+		animated_sprite_2d.play("default")
+	
+	if velocity.y < 0:
+		animated_sprite_2d.play("jumping")
+	if velocity.y > 0:
+		animated_sprite_2d.play("fall")
+	
--- scenes/collectables/chest.gd
+++ scenes/collectables/chest.gd
@@ -3,7 +3,10 @@ extends Area2D
 @onready var game_manager = %gameManager
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
+@export var target_level : PackedScene
+
 func _on_body_entered(body):
 	if (body.name == "CharacterBody2D" && game_manager.key > 0):
 		animated_sprite_2d.play("unlocked")
 		game_manager.remove_key(1)
+		get_tree().change_scene_to_packed(target_level)
